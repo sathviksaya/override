49358a0679c99a02232cd46a947bfa0ef5a63b88
Supreet Ronad <62396592+SupreetRonad@users.noreply.github.com>
2021-08-31 10:39:17 +0530

    created group & event models

---
+++ lib/models/group.dart
@@ -0,0 +1,20 @@
+class Group {
+  late String groupName;
+  late String groupId;
+  late String extension;
+  late String description;
+  late String groupRef;
+
+  Group(
+    String groupName,
+    String groupId,
+    String extension,
+    String description,
+  ) {
+    this.groupName = groupName;
+    this.groupId = groupId;
+    this.extension = extension;
+    this.description = description;
+    this.groupRef = groupId + '###' + extension;
+  }
+}
